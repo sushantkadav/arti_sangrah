4fd8684f78cbbe7202223b23af11165a3d542bdb
sukadav <sushant.sk911@gmail.com>
2022-12-02 10:36:27 +0530

    hindi, sanstrik strot added.

--- lib/model/home/hindi_strot.dart
+++ lib/model/home/hindi_strot.dart
@@ -3,12 +3,18 @@ import 'package:flutter/material.dart';
 class HindiStrot{
   String title;
   String subtile;
-  Widget nextScreen;
+  // Widget nextScreen;
   String imgUrl;
 
 
   HindiStrot({required this.title,
     required this.subtile,
-    required this.nextScreen,
+    // required this.nextScreen,
     required this.imgUrl});
 }
+
+List<HindiStrot> hindiStrotList = [
+  HindiStrot(title: "आरती संग्रह" , subtile: "",  imgUrl: "imgUrl"),
+  HindiStrot(title: "चालीसा संघर्ष" , subtile: "",  imgUrl: "imgUrl"),
+  HindiStrot(title: "भजन स्तुती" , subtile: "",  imgUrl: "imgUrl")
+];
--- lib/screens/home_screen/hindi_strot_list.dart
+++ lib/screens/home_screen/hindi_strot_list.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import  'package:arti_sangrah/model/home/hindi_strot.dart';
 
 class HindiStrotList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HindiStrot")),
-      body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child:  Text("HindiStrot List")
-
+      appBar: AppBar(
+        title: Text("हिंदी स्तोत्र"),
+        backgroundColor: Color(0xFFB73E3E),
       ),
+      body: Container(
+          decoration: new BoxDecoration(color: Color(0xFFFFE9B1)),
+          child: (GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 2,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0),
+              itemCount: hindiStrotList.length,
+              itemBuilder: (context, index) {
+
+                HindiStrot hindiStrot = hindiStrotList[index];
+                return Card(
+                  child: ListTile(
+                    title: Text(
+                      hindiStrot.title,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    subtitle: Text(hindiStrot.subtile.toString()),
+                    leading: Icon(Icons.ac_unit_outlined),
+                    // trailing: Icon(Icons.arrow_forward_rounded),
+                    tileColor: Colors.amberAccent,
+                    // onTap: () {
+                    //   Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //           builder: (context) => arti.nextScreen));
+                    // },
+                  ),
+                );
+              }))),
     );
   }
 }
